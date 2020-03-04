19ec1e7e43e9d696aa2eab4e100719c9ce35cc1b
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-03-04 07:46:28 +0100

    initial commit

---
+++ src/main/java/com/github/maleksandrowicz93/reactivejavaexample/ReactiveJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.maleksandrowicz93.reactivejavaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveJavaExampleApplication.class, args);
+	}
+
+}
